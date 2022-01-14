143ac2cd3ecacc92faf955961182d370af845854
Pranali <55937286+Pranalibit@users.noreply.github.com>
2022-01-14 22:37:20 +0530

    message button changed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -85,19 +85,7 @@ const Card  = ({
                 ></path>
               </svg>
             </button></a></p>'
-            <button className=" relative flex justify-evenly items-center z-10 bg-white border rounded-md p-1 opacity-75 hover:opacity-100 focus:outline-none focus:border-gray-500">
-              <span className="inline-block text-sm px-1 text-gray-600">Contact</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-                className="h-4 w-6 my-1 text-blue-700"
-              >
-                <path
-                  fill="currentColor"
-                  d="M352 320c-22.608 0-43.387 7.819-59.79 20.895l-102.486-64.054a96.551 96.551 0 0 0 0-41.683l102.486-64.054C308.613 184.181 329.392 192 352 192c53.019 0 96-42.981 96-96S405.019 0 352 0s-96 42.981-96 96c0 7.158.79 14.13 2.276 20.841L155.79 180.895C139.387 167.819 118.608 160 96 160c-53.019 0-96 42.981-96 96s42.981 96 96 96c22.608 0 43.387-7.819 59.79-20.895l102.486 64.054A96.301 96.301 0 0 0 256 416c0 53.019 42.981 96 96 96s96-42.981 96-96-42.981-96-96-96z"
-                ></path>
-              </svg>
-            </button>
+           
           </div>
         </div>
       </div>
